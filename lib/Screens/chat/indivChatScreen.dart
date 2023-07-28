6bd9da987346ed59ded9a8a3649912034045cba8
Zeddin Dhia <zeddindhia@Zeddins-MacBook-Air.local>
2023-07-28 01:21:48 +0100

    indiv chat page attach card

--- lib/Screens/chat/indivChatScreen.dart
+++ lib/Screens/chat/indivChatScreen.dart
@@ -2,7 +2,6 @@ import 'package:emoji_selector/emoji_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:heyu_front/Models/ChatModel.dart';
 
-
 class IndivChatScreen extends StatefulWidget {
   const IndivChatScreen({super.key, required this.chatModel});
 
@@ -13,21 +12,22 @@ class IndivChatScreen extends StatefulWidget {
 }
 
 class _IndivChatScreenState extends State<IndivChatScreen> {
-  bool showEmojis=false;
-  FocusNode focusNode=FocusNode();
-  TextEditingController textEditingController=TextEditingController();
+  bool showEmojis = false;
+  FocusNode focusNode = FocusNode();
+  TextEditingController textEditingController = TextEditingController();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     focusNode.addListener(() {
-      if(focusNode.hasFocus){
+      if (focusNode.hasFocus) {
         setState(() {
-          showEmojis=false;
+          showEmojis = false;
         });
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +43,7 @@ class _IndivChatScreenState extends State<IndivChatScreen> {
                 onTap: () {
                   Navigator.pop(context);
                 },
-                child: Icon(
+                child: const Icon(
                   Icons.arrow_back,
                   size: 24,
                 )),
@@ -57,16 +57,17 @@ class _IndivChatScreenState extends State<IndivChatScreen> {
         title: InkWell(
           onTap: () {}, //for the contact profile
           child: Container(
-            margin: EdgeInsets.all(5),
+            margin: const EdgeInsets.all(5),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   widget.chatModel.users[1].userName,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                Text(
+                const Text(
                   "last seen today at 18:18",
                   style: TextStyle(
                     fontSize: 13,
@@ -77,29 +78,29 @@ class _IndivChatScreenState extends State<IndivChatScreen> {
           ),
         ),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.video_call)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.call)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.video_call)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
           PopupMenuButton<String>(
             itemBuilder: (BuildContext context) {
               return [
-                PopupMenuItem(
+                const PopupMenuItem(
                   child: Text("Profile"),
                   value: "profile",
                 ),
-                PopupMenuItem(
-                  child: Text("New brodcast"),
+                const PopupMenuItem(
                   value: "brodcast",
+                  child: const Text("New brodcast"),
                 ),
-                PopupMenuItem(
-                  child: Text("Archive"),
+                const PopupMenuItem(
+                  child: const Text("Archive"),
                   value: "archive",
                 ),
-                PopupMenuItem(
-                  child: Text("Settings"),
+                const PopupMenuItem(
+                  child: const Text("Settings"),
                   value: "settings",
                 ),
-                PopupMenuItem(
-                  child: Text("Logout"),
+                const PopupMenuItem(
+                  child: const Text("Logout"),
                   value: "logout",
                 ),
               ];
@@ -125,8 +126,11 @@ class _IndivChatScreenState extends State<IndivChatScreen> {
                   Row(
                     children: [
                       Container(
+                        width: MediaQuery.of(context).size.width - 60,
                         child: Card(
-                          margin: EdgeInsets.only(left: 3, bottom: 8),
+                          margin: const EdgeInsets.only(left: 3, bottom: 8),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)),
                           child: TextFormField(
                             controller: textEditingController,
                             focusNode: focusNode,
@@ -138,39 +142,42 @@ class _IndivChatScreenState extends State<IndivChatScreen> {
                               border: InputBorder.none,
                               hintText: "Type a message",
                               prefixIcon: IconButton(
-                                icon: Icon(Icons.emoji_emotions),
-                                onPressed: (){
+                                icon: const Icon(Icons.emoji_emotions),
+                                onPressed: () {
                                   focusNode.unfocus();
-                                  focusNode.canRequestFocus=false;
+                                  focusNode.canRequestFocus = false;
                                   setState(() {
-                                    showEmojis=!showEmojis;
+                                    showEmojis = !showEmojis;
                                   });
                                 },
                               ),
-
                               suffixIcon: Row(
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
                                   IconButton(
-                                    icon: Icon(Icons.attach_file),
-                                    onPressed: () {},
+                                    icon: const Icon(Icons.attach_file),
+                                    onPressed: () {
+                                      showModalBottomSheet(
+                                        context: context,
+                                        builder: (builder) => bottomSheet(),
+                                        backgroundColor: Colors.transparent,
+                                      );
+                                    },
                                   ),
                                   IconButton(
-                                    icon: Icon(Icons.camera_alt),
+                                    icon: const Icon(Icons.camera_alt),
                                     onPressed: () {},
                                   ),
                                 ],
                               ),
-                              contentPadding: EdgeInsets.all(5),
+                              contentPadding: const EdgeInsets.all(5),
                             ),
                           ),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25)),
                         ),
-                        width: MediaQuery.of(context).size.width - 60,
                       ),
                       Padding(
-                        padding: const EdgeInsets.only(bottom: 8,right: 3,left: 3),
+                        padding:
+                            const EdgeInsets.only(bottom: 8, right: 3, left: 3),
                         child: CircleAvatar(
                           backgroundColor: Colors.pink,
                           radius: 25,
@@ -185,7 +192,7 @@ class _IndivChatScreenState extends State<IndivChatScreen> {
                       ),
                     ],
                   ),
-                  showEmojis?emojiSelect():Container(),
+                  showEmojis ? emojiSelect() : Container(),
                 ],
               ),
             )
@@ -196,15 +203,78 @@ class _IndivChatScreenState extends State<IndivChatScreen> {
   }
 
   Widget emojiSelect() {
-    return  EmojiSelector(
-           onSelected: (emoji) {
-             print(emoji.char) ;
-             setState(() {
-               textEditingController.text=textEditingController.text+emoji.char;
-             });
-             },
-          /*padding: EdgeInsets.only(top: 60),*/
+    return EmojiSelector(
+      onSelected: (emoji) {
+        print(emoji.char);
+        setState(() {
+          textEditingController.text = textEditingController.text + emoji.char;
+        });
+      },
+      /*padding: EdgeInsets.only(top: 60),*/
+    );
+  }
 
+  Widget bottomSheet() {
+    return Container(
+      height: 278,
+      width: MediaQuery.of(context).size.width,
+      child: Card(
+        margin: EdgeInsets.all(18),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  iconCreation(Icons.insert_drive_file,Colors.blue,"Document"),
+                  const SizedBox(width: 40,),
+                  iconCreation(Icons.camera_alt,Colors.pink,"Camera"),
+                  const SizedBox(width: 40,),
+                  iconCreation(Icons.insert_photo,Colors.purple,"Gallery"),
+                ],
+              ),
+              const SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  iconCreation(Icons.headset,Colors.orange,"Audio"),
+                  const SizedBox(width: 40,),
+                  iconCreation(Icons.location_pin,Colors.cyan,"Location"),
+                  const SizedBox(width: 40,),
+                  iconCreation(Icons.person,Colors.lightGreen,"Contact"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget iconCreation(IconData icon, Color color, String text) {
+    return InkWell(
+      onTap: (){},
+      child: Column(
+        children: [
+          CircleAvatar(
+            backgroundColor: color,
+            radius: 30,
+            child: Icon(
+              icon,
+              size: 29,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 5,),
+          Text(
+              text,
+            style: const TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
